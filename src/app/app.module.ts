1a7bd4ac218da6260c733669ab01c4a632abff38
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-11-09 23:49:08 -0500

    Project adjustment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/components/product/product.component';
-import { CartComponent } from './cart/cart.component';
-import { ProductsComponent } from './product/components/products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product/components/product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 import { CoreModule } from './core/core.module';
 import { ProductModule } from './product/product.module';
@@ -19,7 +15,6 @@ import { ProductModule } from './product/product.module';
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
     ContactComponent,
     DemoComponent,
     PageNotFoundComponent,
--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from 'src/app/product.model';
+import { Product } from 'src/app/core/models/product.model';
 
 @Component({
   selector: 'app-product',
