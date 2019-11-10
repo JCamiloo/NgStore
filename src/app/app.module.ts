450e368992467a7f3a482d5b15b488710791a049
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-11-10 13:29:51 -0500

    modularization finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
-import { SharedModule } from './shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
-import { DemoComponent } from './demo/demo.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 import { CoreModule } from './core/core.module';
-import { ProductModule } from './product/product.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent,
-    DemoComponent,
-    PageNotFoundComponent,
     LayoutComponent
   ],
   imports: [
@@ -25,8 +17,7 @@ import { ProductModule } from './product/product.module';
     AppRoutingModule,
     FormsModule,
     SharedModule,
-    CoreModule,
-    ProductModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,19 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoRoutingModule } from './demo-routing.module';
+import { DemoComponent } from './components/demo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    DemoComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    DemoRoutingModule,
+    SharedModule
+  ]
+})
+export class DemoModule { }
