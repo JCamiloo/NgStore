7b8fd2b81c47647b60ee27c856a418d574462f43
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-11-19 20:28:29 -0500

    handling errors with rxjs

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from 'src/app/core/models/product.model';
 import { environment } from './../../../environments/environment';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +13,27 @@ export class ProductsService {
   constructor(private http: HttpClient) { }
 
   getAllProducts() {
-    return this.http.get<Product[]>(`${environment.url_api}/products/`);
+    return this.http.get<Product[]>(`${environment.url_api}/products/`).pipe(catchError(this.handleError));
   } 
 
   getProduct(id: string) {
-    return this.http.get<Product>(`${environment.url_api}/products/${id}`);
+    return this.http.get<Product>(`${environment.url_api}/products/${id}`).pipe(catchError(this.handleError));
   }
 
   createProduct(product: Product) {
-    return this.http.post(`${environment.url_api}/products`, product);
+    return this.http.post(`${environment.url_api}/products`, product).pipe(catchError(this.handleError));
   }
 
   updateProduct(id: string, changes: Partial<Product>) {
-    return this.http.put<Product>(`${environment.url_api}/products/${id}`, changes);
+    return this.http.put<Product>(`${environment.url_api}/products/${id}`, changes).pipe(catchError(this.handleError));
   }
 
   deleteProduct(id: string) {
-    return this.http.delete(`${environment.url_api}/products/${id}`);
+    return this.http.delete(`${environment.url_api}/products/${id}`).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+    return throwError('Algo salió mal');
   }
 }
